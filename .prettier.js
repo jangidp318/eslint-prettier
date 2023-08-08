673015c25e086e931196762aa9451a6f8c05d991
Pankaj Sharma <85607879+jangidp318@users.noreply.github.com>
2023-08-08 18:38:55 +0530

    Create .prettier.js

---
+++ .prettier.js
@@ -0,0 +1,13 @@
+module.exports = {
+  arrowParens: 'always',
+  bracketSameLine: true,
+  singleQuote: true,
+  trailingComma: 'es5',
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  jsxSingleQuote: false,
+  quoteProps: 'as-needed',
+  semi: false,
+  printWidth: 100,
+  tabWidth: 2,
+}
